f8f2b363fea69156fb99c6a022724ec55d288563
Jasper Horn <jasperhorn@gmail.com>
2017-02-09 00:46:02 +0100

    [#12] First implementation of get capabilities

--- main/main.ino
+++ main/main.ino
@@ -25,6 +25,7 @@ volatile const uint8_t* response;
 
 const uint8_t pingResponse[] = { 0x01, 0x01 };
 const uint8_t listPinsResponse[] = { 0x04, 0x01, 0x01, 0x03, 0x05 };
+const uint8_t listCapabilitiesResponse[] = { 0x08, 0x00, 0xEF, 0x01, 0xEF, 0xFF, 0x05, 0xFF, 0xFF };
 const uint8_t deviceIdentifierResponse[] = { 0x01, 0x01 };
 const uint8_t deviceVersionResponse[] = { 0x01, 0x80 };
 
@@ -61,6 +62,11 @@ void receiveEvent(uint8_t howMany)
                     response = listPinsResponse;
                     responsePosition = 0;
                     responseLength = sizeof(listPinsResponse);
+                    break;
+                case 0x03:
+                    response = listCapabilitiesResponse;
+                    responsePosition = 0;
+                    responseLength = sizeof(listCapabilitiesResponse);
                     break;
                 case 0x04:
                     response = deviceIdentifierResponse;
