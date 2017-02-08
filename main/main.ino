6782e7dd491cec7dbb4c7497391cfe246bf9bf04
Jasper Horn <jasperhorn@gmail.com>
2017-02-09 00:24:02 +0100

    [#12] First implementation of list pins

--- main/main.ino
+++ main/main.ino
@@ -24,6 +24,7 @@ int n = 0;
 volatile const uint8_t* response;
 
 const uint8_t pingResponse[] = { 0x01, 0x01 };
+const uint8_t listPinsResponse[] = { 0x04, 0x01, 0x01, 0x03, 0x05 };
 
 volatile byte responsePosition;
 volatile byte responseLength;
@@ -52,7 +53,12 @@ void receiveEvent(uint8_t howMany)
                 case 0x01:
                     response = pingResponse;
                     responsePosition = 0;
-                    responseLength = 2;
+                    responseLength = sizeof(pingResponse);
+                    break;
+                case 0x02:
+                    response = listPinsResponse;
+                    responsePosition = 0;
+                    responseLength = sizeof(listPinsResponse);
                     break;
                 default:
                     // unknown command
