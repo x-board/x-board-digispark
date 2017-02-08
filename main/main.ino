a3d1b620bacf5709712f05ae9a53f445353e2759
Jasper Horn <jasperhorn@gmail.com>
2017-02-09 00:15:40 +0100

    [#12] First implementation of ping

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,68 @@
 
-#include "Wire.h"
+#include "TinyWireS.h"
+
+#define LED 1
+
+bool led_on = true;
 
 void setup()
 {
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, HIGH);
+    
+    TinyWireS.begin(36);
+    TinyWireS.onRequest(requestEvent);
+    TinyWireS.onReceive(receiveEvent);
 }
 
 void loop()
 {
 }
+
+int n = 0;
+
+volatile const uint8_t* response;
+
+const uint8_t pingResponse[] = { 0x01, 0x01 };
+
+volatile byte responsePosition;
+volatile byte responseLength;
+
+
+void requestEvent()
+{
+    if (responsePosition < responseLength)
+    {
+        TinyWireS.send(response[responsePosition]);
+        responsePosition++;
+    }
+}
+
+void receiveEvent(uint8_t howMany)
+{
+    switch (TinyWireS.receive())
+    {
+        case 0x00:
+            if (howMany < 2)
+            {
+                return;
+            }
+            switch (TinyWireS.receive())
+            {
+                case 0x01:
+                    response = pingResponse;
+                    responsePosition = 0;
+                    responseLength = 2;
+                    break;
+                default:
+                    // unknown command
+                    break;
+            }
+            
+            break;
+        default:
+            // unknown command
+            break;
+    }
+}
+
