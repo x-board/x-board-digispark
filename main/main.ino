c4d9017bcd56f4dad9ea5c464213220848c5f40d
Jasper Horn <jasperhorn@gmail.com>
2017-02-09 01:10:42 +0100

    [#16] First implementation of digital write

--- main/main.ino
+++ main/main.ino
@@ -25,7 +25,7 @@ volatile const uint8_t* response;
 
 const uint8_t pingResponse[] = { 0x01, 0x01 };
 const uint8_t listPinsResponse[] = { 0x04, 0x01, 0x01, 0x03, 0x05 };
-const uint8_t listCapabilitiesResponse[] = { 0x08, 0x00, 0xEF, 0x01, 0xEF, 0xFF, 0x05, 0xFF, 0xFF };
+const uint8_t listCapabilitiesResponse[] = { 11, 0x00, 0xEF, 0x01, 0xEF, 0xFF, 0x05, 0xFF, 0x01, 0x01, 0xFF, 0xFF };
 const uint8_t deviceIdentifierResponse[] = { 0x01, 0x01 };
 const uint8_t deviceVersionResponse[] = { 0x01, 0x80 };
 
@@ -81,6 +81,38 @@ void receiveEvent(uint8_t howMany)
                 default:
                     // unknown command
                     break;
+            }
+            break;
+            
+        case 0x01:
+            if (howMany < 2)
+            {
+                return;
+            }
+            switch (TinyWireS.receive())
+            {
+                case 0x01:
+                {
+                    if (howMany < 4)
+                    {
+                        return;
+                    }
+                    uint8_t pin = TinyWireS.receive();
+                    uint8_t value = TinyWireS.receive();
+
+                    if (pin == 0 || pin == 2 || pin > 5 || (value != 0 && value != 1))
+                    {
+                        return;
+                    }
+
+                    digitalWrite(pin, value == 1 ? HIGH : LOW);
+                    break;
+                }
+                default:
+                {
+                    // unknown command
+                    break;
+                }
             }
             
             break;
