ece0eb3a09dfbd141a95ef2f255167a98babc36c
Jasper Horn <jasperhorn@gmail.com>
2017-12-31 00:22:01 +0100

    [#7] Add arduino project to identify pins

---
+++ identify-pins/identify-pins.ino
@@ -0,0 +1,34 @@
+void setup() {
+    pinMode(1, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+}
+
+void loop() {
+    for (int i = 1; i < 6; i++)
+    {
+        for (int pin = i; pin < 6; pin++)
+        {
+            if (pin != 2)
+            {
+                digitalWrite(pin, HIGH);
+            }
+        }
+
+        delay(200);
+
+        for (int pin = i; pin < 6; pin++)
+        {
+            if (pin != 2)
+            {
+                digitalWrite(pin, LOW);
+            }
+        }
+        
+        delay(200);
+    }
+
+    delay(1000);
+
+}
